944431eef2f2afa939a8fe0d5b0f1759851e801d
Matt Culpepper <matt@culpepper.co>
2020-07-02 15:31:13 -0500

    Add API

---
+++ apps/api/controllers/counties/index.rb
@@ -0,0 +1,49 @@
+module Api
+  module Controllers
+    module Counties
+      class Index
+        include Api::Action
+
+        def call(params)
+          @counties = CountyRepository.new.all_with_updates.to_a
+
+          self.body = JSON.dump(build_hash)
+          self.format = :json
+        end
+
+        # TODO: clean this up
+        def build_hash
+          @counties.map do |county|
+            {
+              county: {
+                name: county.name,
+                updates: county.county_updates.map do |county_update|
+                  county_update_hash = {
+                    date: county_update.date.to_date,
+                    total: {
+                      cases: county_update.cases,
+                      deaths: county_update.deaths,
+                      ltc_cases: county_update.ltc_cases,
+                      ltc_deaths: county_update.ltc_deaths
+                    }
+                  }
+
+                  if county_update.previous_update
+                    county_update_hash[:new] = {
+                      cases: county_update.cases - county_update.previous_update.cases,
+                      deaths: county_update.deaths - county_update.previous_update.deaths,
+                      ltc_cases: county_update.ltc_cases - county_update.previous_update.ltc_cases,
+                      ltc_deaths: county_update.ltc_deaths - county_update.previous_update.ltc_deaths
+                    }
+                  end
+
+                  county_update_hash
+                end
+              }
+            }
+          end
+        end
+      end
+    end
+  end
+end
--- config/environment.rb
+++ config/environment.rb
@@ -3,8 +3,10 @@ require 'hanami/setup'
 require 'hanami/model'
 require_relative '../lib/covid'
 require_relative '../apps/web/application'
+require_relative '../apps/api/application'
 
 Hanami.configure do
+  mount Api::Application, at: '/api/v1'
   mount Web::Application, at: '/'
 
   model do
