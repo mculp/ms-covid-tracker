d14b8d59be14faf1114d05a56a32486439759c42
Matt Culpepper <matt@culpepper.co>
2020-07-20 10:03:31 -0500

    Add percent change to template.

--- apps/web/views/counties/index.rb
+++ apps/web/views/counties/index.rb
@@ -7,6 +7,16 @@ module Web
         def updated
           CountyUpdateRepository.new.last.date.strftime("%B %d, %Y")
         end
+
+        def badge_class(percent)
+          if percent > 10
+            'badge-danger'
+          elsif percent > 5
+            'badge-warning'
+          else
+            'badge-info'
+          end
+        end
       end
     end
   end
